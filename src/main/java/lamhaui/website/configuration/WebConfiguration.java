fd82b148701e44bbc3088ca4c24d2ea1353bea13
huylam98it <huylam98it@gmail.com>
2019-11-22 16:41:33 +0700

    init

---
+++ src/main/java/lamhaui/website/configuration/WebConfiguration.java
@@ -0,0 +1,18 @@
+package lamhaui.website.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lamhaui.website")
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+}
